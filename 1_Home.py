4f3c77da09cf21dc673a18110734716371832218
Bharathkumar M S <96257624+Bharathkumar-ms@users.noreply.github.com>
2023-10-09 18:59:08 +0530

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -15,10 +15,6 @@ st.markdown(
 
 ###### The primary objective of this project is to detect and classify plant diseases at an early stage. Early detection can help farmers take timely action to prevent the spread of diseases and minimize crop loss.
 
-### Motive
-
-###### For this challenge, we used the “plant_disease” dataset. This dataset contains an open access repository of images on plant health to enable the development of web disease diagnostics. The dataset contains 87,0000 images. The images span 14 crop species: Apple, Blueberry, Cherry, Grape, Orange, Peach, Bell Pepper, Potato, Raspberry, Soybean, Squash, Strawberry, and Tomato. It contains images of 17 fundal diseases, 4 bacterial diseases, 2 molds (oomycete) diseases, 2 viral diseases, and 1 disease caused by a mite. 12 crop species also have images of healthy leaves that are not visibly affected by a disease.
-
 
 '''
 )
